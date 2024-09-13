a8976f0a0b34cd35371afd0e03e7a432c76fb6de
unknown <andreytsvl15@gmail.com>
2024-09-13 10:06:10 +0300

    sem_7_1

--- Program.cs
+++ Program.cs
@@ -48,4 +48,76 @@
 // task 46 Задайте двумерный массив размерами м*н,
 // заполненный случайными числами
 
+using System;
+using static System.Console;
 
+// Clear();
+// Write("Введите размер массива через пробел ");
+// string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+// int[,] array = GetArray(int.Parse(nums[0]), int.Parse(nums[1]),-10,10);
+// PrintArray(array);
+
+// int[,] GetArray(int rows, int columns, int min, int max)
+// {
+//     int[,] result = new int[rows, columns];
+//     for (int i = 0; i < result.GetLength(0); i++) // метод возвращает значение ровс (на нулевой позиции)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Write($"{inArray[i, j]} ");
+
+//         }
+//         WriteLine();
+//     }
+
+// }
+
+// task 48 Задайте двумерный массив размера м на н
+// каждый элемент в массиве находится по формуле Амн = м+н
+
+Clear();
+Write("Введите размер массива через пробел ");
+string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,] array = GetArray(int.Parse(nums[0]), int.Parse(nums[1]));
+PrintArray(array);
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < result.GetLength(0); i++) // метод возвращает значение ровс (на нулевой позиции)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+
+        }
+        WriteLine();
+    }
+
+}
