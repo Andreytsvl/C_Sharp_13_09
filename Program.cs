614d3ff0992e557a12ce388936cf676d22aa8583
unknown <andreytsvl15@gmail.com>
2024-09-13 10:57:51 +0300

    sem_7_2

--- Program.cs
+++ Program.cs
@@ -88,36 +88,119 @@ using static System.Console;
 // task 48 Задайте двумерный массив размера м на н
 // каждый элемент в массиве находится по формуле Амн = м+н
 
+// Clear();
+// Write("Введите размер массива через пробел ");
+// string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+// int[,] array = GetArray(int.Parse(nums[0]), int.Parse(nums[1]));
+// PrintArray(array);
+
+// int[,] GetArray(int rows, int columns)
+// {
+//     int[,] result = new int[rows, columns];
+//     for (int i = 0; i < result.GetLength(0); i++) // метод возвращает значение ровс (на нулевой позиции)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             result[i, j] = i + j;
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Write($"{inArray[i, j]} ");
+
+//         }
+//         WriteLine();
+//     }
+
+// }
+
+// task 49
+// Задайте двумерный массив. Найдите эл-ты, у которых оба индекса
+// чётные , и замените эти эл-ты на их квадраты
+// Clear();
+// Write("Введите размер массива через пробел ");
+// string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+// int[,] array = GetArray(int.Parse(nums[0]), int.Parse(nums[1]), 0, 10);
+// PrintArray(array);
+// int[,] array1 = SqArray(array); // мой способ
+// PrintArray(array1); // мой способ
+// ChangeArrey(array);
+// PrintArray(array);
+
+// int[,] GetArray(int rows, int columns, int min, int max)
+// {
+//     int[,] result = new int[rows, columns];
+//     for (int i = 0; i < result.GetLength(0); i++) // метод возвращает значение ровс (на нулевой позиции)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// int[,] SqArray(int[,] inArray) // мой способ
+// {
+//     int[,] result1 = new int[inArray.GetLength(0), inArray.GetLength(1)];
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             if ((i % 2 == 0) && (j % 2 == 0) && (i > 0) && (j > 0))
+//             {
+//                 result1[i, j] = array[i, j] * array[i, j];
+//             }
+//             else
+//             {
+//                 result1[i, j] = array[i, j];
+//             }
+//         }
+//     }
+//     return result1;
+// }
+
+// void ChangeArrey(int[,] inArray) //второй способ
+// {
+//     for (int i = 2; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 2; j < inArray.GetLength(1); j++)
+//         {
+//             if ((i % 2 == 0) && (j % 2 == 0))
+//             {
+//                 array[i, j] = array[i, j] * array[i, j];
+//             }
+            
+//         }
+//     }
+// }
+
+
+// void PrintArray(int[,] inArray)
+// {
+
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Write($"{inArray[i, j]} ");
+
+//         }
+//         WriteLine();
+//     }
+//     WriteLine();
+
+// }
+
+// task 51 Задайте Двумерный массив
+// Найдите сумму элементов на главной диагонали
+// (с индексами (0;0);(1;1) и т.д.)
 Clear();
-Write("Введите размер массива через пробел ");
-string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-int[,] array = GetArray(int.Parse(nums[0]), int.Parse(nums[1]));
-PrintArray(array);
-
-int[,] GetArray(int rows, int columns)
-{
-    int[,] result = new int[rows, columns];
-    for (int i = 0; i < result.GetLength(0); i++) // метод возвращает значение ровс (на нулевой позиции)
-    {
-        for (int j = 0; j < result.GetLength(1); j++)
-        {
-            result[i, j] = i + j;
-        }
-    }
-    return result;
-}
-
-void PrintArray(int[,] inArray)
-{
-
-    for (int i = 0; i < inArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            Write($"{inArray[i, j]} ");
-
-        }
-        WriteLine();
-    }
-
-}
